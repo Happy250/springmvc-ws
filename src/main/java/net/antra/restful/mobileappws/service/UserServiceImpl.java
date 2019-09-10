f64ac2f6edcfba6bcc07fc4a853981aa042cfd05
Happy250 <chai.yi@husky.neu.edu>
2019-09-10 17:50:20 -0400

    finished RESTful get method

--- src/main/java/net/antra/restful/mobileappws/service/UserServiceImpl.java
+++ src/main/java/net/antra/restful/mobileappws/service/UserServiceImpl.java
@@ -53,6 +53,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserDto getUserByUserId(String id) {
+        UserDto returnValue = new UserDto();
+        UserEntity userEntity = userRepository.findByUserId(id);
+
+        if (userEntity == null) throw new UsernameNotFoundException(id);
+        BeanUtils.copyProperties(userEntity, returnValue);
+        
+        return returnValue;
+    }
+
+    @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity ue = userRepository.findUserByEmail(email);
         if (ue == null) throw new UsernameNotFoundException(email);
