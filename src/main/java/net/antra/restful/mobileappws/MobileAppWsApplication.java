64d03343d46e55d0a1bdcef44b5efa0b5dd8a767
Happy250 <chai.yi@husky.neu.edu>
2019-09-11 15:23:08 -0400

    figure on deploy on tomcat

--- src/main/java/net/antra/restful/mobileappws/MobileAppWsApplication.java
+++ src/main/java/net/antra/restful/mobileappws/MobileAppWsApplication.java
@@ -3,11 +3,18 @@ package net.antra.restful.mobileappws;
 import net.antra.restful.mobileappws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applciation) {
+        return applciation.sources(MobileAppWsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MobileAppWsApplication.class, args);
